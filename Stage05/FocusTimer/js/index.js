5301f559e17a1857d5aa19e6d6e659b97030cd53
Dionatan de Lima <dionatanlm12271@gmail.com>
2023-04-12 22:58:15 -0300

    feat: added challenge

---
+++ Stage05/FocusTimer/js/index.js
@@ -0,0 +1,70 @@
+import Controls from "./controls.js";
+import Timer from "./timer.js";
+import Sound from "./sounds.js";
+import {
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js";
+
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+});
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+});
+
+const sound = Sound();
+
+buttonPlay.addEventListener("click", () => {
+  controls.play();
+  timer.countdown();
+  sound.pressButton();
+});
+
+buttonPause.addEventListener("click", () => {
+  controls.pause();
+  timer.hold();
+  sound.pressButton();
+});
+
+buttonStop.addEventListener("click", () => {
+  controls.reset();
+  timer.reset();
+  sound.pressButton();
+});
+
+buttonSoundOn.addEventListener("click", () => {
+  buttonSoundOn.classList.add("hide");
+  buttonSoundOff.classList.remove("hide");
+  sound.bgAudio.play();
+});
+
+buttonSoundOff.addEventListener("click", () => {
+  buttonSoundOn.classList.remove("hide");
+  buttonSoundOff.classList.add("hide");
+  sound.bgAudio.pause();
+});
+
+buttonSet.addEventListener("click", () => {
+  let newMinutes = controls.getMinutes();
+
+  if (!newMinutes) {
+    timer.reset();
+    return;
+  }
+
+  timer.updateDisplay(newMinutes, 0);
+  timer.updateMinutes(newMinutes);
+});
